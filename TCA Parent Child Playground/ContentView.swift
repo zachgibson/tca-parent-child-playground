ca1c2847d9c406fd956749abbc818c0ff16ed0ac
Zachary Gibson <zackgib@aol.com>
2023-12-17 11:42:27 -0600

    Initial Commit

---
+++ TCA Parent Child Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA Parent Child Playground
+//
+//  Created by Zachary Gibson on 12/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA Parent Child Playground/TCA_Parent_Child_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Parent_Child_PlaygroundApp.swift
+//  TCA Parent Child Playground
+//
+//  Created by Zachary Gibson on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Parent_Child_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
